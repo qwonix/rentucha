d0e0e09ba7b23ffbcc9516a6c432ea1087b44fd1
Roman <roman.qwonix@gmail.com>
2022-12-06 20:16:47 +0300

    init commit

---
+++ src/test/java/ru/qwonix/rentucha/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.qwonix.rentucha.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
