df4045512cc33e73df56e40ad8ca6edece2f8ad9
Roman <roman.qwonix@gmail.com>
2023-01-08 15:37:42 +0300

    remove unnecessary fragment

--- app/src/main/java/ru/qwonix/android/rentucha/fragments/search/SearchBarFragment.kt
+++
@@ -1,7 +0,0 @@
-package ru.qwonix.android.rentucha.fragments.search
-
-import androidx.fragment.app.Fragment
-import ru.qwonix.android.rentucha.R
-
-class SearchBarFragment : Fragment(R.layout.fragment_search_bar) {
-}
