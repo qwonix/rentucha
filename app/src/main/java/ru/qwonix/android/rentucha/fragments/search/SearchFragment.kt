36d5b10c9a196f5c969c3a069ce975afabfea457
Roman <roman.qwonix@gmail.com>
2023-01-16 10:06:14 +0300

    bind searchQuery for settings fragment; add view binding

--- app/src/main/java/ru/qwonix/android/rentucha/fragments/search/SearchFragment.kt
+++ app/src/main/java/ru/qwonix/android/rentucha/fragments/search/SearchFragment.kt
@@ -2,7 +2,9 @@ package ru.qwonix.android.rentucha.fragments.search
 
 import android.content.Context
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.view.ViewTreeObserver
 import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
@@ -10,19 +12,36 @@ import android.widget.EditText
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.findNavController
-import com.google.android.material.textfield.TextInputEditText
 import ru.qwonix.android.rentucha.R
+import ru.qwonix.android.rentucha.databinding.FragmentSearchBinding
 
 class SearchFragment : Fragment(R.layout.fragment_search) {
+    private lateinit var binding: FragmentSearchBinding
     private val sharedSearchSettingsViewModel: SearchSettingsViewModel by activityViewModels()
+
+    override fun onResume() {
+        super.onResume()
+        binding.editTextSearchQuery.setText(sharedSearchSettingsViewModel.getSearchQueryOrElseEmptyStrings())
+        // set cursor to end
+        binding.editTextSearchQuery.setSelection(binding.editTextSearchQuery.length())
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding =
+            FragmentSearchBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val editTextSearchQuery = view.findViewById<TextInputEditText>(R.id.edit_text_search_query)
-        editTextSearchQuery.focusAndShowKeyboard()
-        editTextSearchQuery.onSearch {
-            sharedSearchSettingsViewModel.setSearchQuery(editTextSearchQuery.text.toString())
+        binding.editTextSearchQuery.focusAndShowKeyboard()
+        binding.editTextSearchQuery.onSearch {
+            sharedSearchSettingsViewModel.setSearchQuery(binding.editTextSearchQuery.text.toString())
 
             view.findNavController()
                 .navigate(R.id.action_searchFragment_to_searchSettingsWhereExpandedFragment)
